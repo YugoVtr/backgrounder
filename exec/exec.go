bea1384e251a552d70cabfe59353f16204476a3b
yugovtr <vtrhg69@hotmail.com>
2021-09-09 00:34:23 -0300

    :memo: (documentation) describe exec adapter

--- exec/exec.go
+++ exec/exec.go
@@ -5,12 +5,17 @@ import (
 	"os/exec"
 )
 
+// Commander is an abstrataction to execute the named program with
+// the given arguments.
 type Commander func(s string, a ...string) Cmd
 
+// Command is an adapter to os/exec.Command
+// returning an abstraction rather than a concrete implementation.
 func Command(s string, a ...string) Cmd {
 	return exec.Command(s, a...)
 }
 
+// Cmd represents an external command being prepared or run.
 type Cmd interface {
 	Run() error
 	Output() ([]byte, error)
